5e4469bfdc63e6f757dabe099e641780256bc4d7
Mat Pataki <matpataki@gmail.com>
2016-09-06 07:45:17 -0600

    try using app root in unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,16 +1,18 @@
+APP_ROOT = File.expand_path(File.dirname(File.dirname(__FILE__)))
+
 # Set the working application directory
 # working_directory "/path/to/your/app"
-working_directory "/var/www/webhooks"
+working_directory APP_ROOT
 
 # Unicorn PID file location
 # pid "/path/to/pids/unicorn.pid"
-pid "/var/www/webhooks/pids/unicorn.pid"
+pid "#{APP_ROOT}/pids/unicorn.pid"
 
 # Path to logs
 # stderr_path "/path/to/log/unicorn.log"
 # stdout_path "/path/to/log/unicorn.log"
-stderr_path "/var/www/webhooks/log/unicorn.log"
-stdout_path "/var/www/webhooks/log/unicorn.log"
+stderr_path "#{APP_ROOT}/log/unicorn.log"
+stdout_path "#{APP_ROOT}/log/unicorn.log"
 
 # Unicorn socket
 listen "/tmp/unicorn.webhooks.sock"
